388b7d7196ce8d11ee95a6699ac099f3193df150
Adam <adamlapacz@gmail.com>
2021-07-05 22:29:46 +0200

    firebase added

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from "react";
+// import firebase from "firebase";
+import {db} from "../firebase";
 import {AddPlant} from "./addPlant";
 import {PlantsList} from "./plantsList";
-export const API = 'http://localhost:3000';
+
+//export const API = 'http://localhost:3000';
 
 
 export const Home = () => {
@@ -13,42 +16,90 @@ export const Home = () => {
     }, [])
 
     const getPlants = () => {
-        fetch(`${API}/plants`, {
-            method: 'GET'
-        })
-            .then(resp => resp.json())
-            .then(plants => {
-                console.log('plants::', plants);
-                setPlants(plants);
+        db.collection('plants')
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach(plt => {
+                    const data = (doc) => {
+                        return {id: doc.id, ...doc.data()}
+                    };
+                    setPlants(plants => [...plants, data(plt)])
+                })
             })
-            .catch(err => console.log('Err', err));
+            .catch(error => console.error('Err', error))
     }
 
     const addPlant = (plant) => {
-        fetch(`${API}/plants`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(plant)
-        })
-            .then(resp => resp.json())
-            .then(newPlant => {
+        db.collection('plants')
+            .add(plant)
+            .then(() => {
+               // alert('Roślina dodana')
                 setPlants([
                     ...plants,
-                    newPlant
+                    plant
                 ])
             })
+            .catch(error => console.error('error', error))
     }
 
     const deletePlant = (plantId) => {
+        db.collection('plants')
+            .doc(plantId)
+            .delete()
+            .then(() => setPlants(plants.filter(plant => plant.id !== plantId)))
+            .catch(error => console.error('error', error))
+
+        /*fetch(`${API}/plants/${plantId}`, {
+            method: "DELETE"
+        })
+            .then(resp => {
+                if (resp.ok) {
+                    setPlants(plants.filter(plant => plant.id !== plantId))
+                }
+                throw new Error();
+            })*/
+    }
+
+    /* const getPlants = () => {
+         fetch(`${API}/plants`, {
+             method: 'GET'
+         })
+             .then(resp => resp.json())
+             .then(plants => {
+                 console.log('plants::', plants);
+                 setPlants(plants);
+             })
+             .catch(err => console.log('Err', err));
+     }*/
+
+    /* const addPlant = (plant) => {
+         fetch(`${API}/plants`, {
+             method: 'POST',
+             headers: {
+                 'Content-Type': 'application/json'
+             },
+             body: JSON.stringify(plant)
+         })
+             .then(resp => resp.json())
+             .then(newPlant => {
+                 setPlants([
+                     ...plants,
+                     newPlant
+                 ])
+             })
+     }*/
+
+   /* const deletePlant = (plantId) => {
         fetch(`${API}/plants/${plantId}`, {
             method: "DELETE"
         })
             .then(resp => {
-                setPlants(plants.filter(plant => plant.id !== plantId))
+                if (resp.ok) {
+                    setPlants(plants.filter(plant => plant.id !== plantId))
+                }
+                throw new Error();
             })
-    }
+    }*/
 
     const handleOpenAdd = todo => {
         //  e.preventDefault();
@@ -58,8 +109,11 @@ export const Home = () => {
     return (
         <>
             <h1 className='home__title'>Moje rośliny:</h1>
-            {!plants.length ? <h2>Wczytuję dane..</h2> : <PlantsList showPlants={plants} openAdd={handleOpenAdd} onDelete={deletePlant}/>}
+            {!plants.length ? <div><h2>Wczytuję dane..</h2>
+                <button onClick={() => setOpenAdd(true)} className='plant__add'>Dodaj</button>
+            </div> : <PlantsList showPlants={plants} openAdd={handleOpenAdd} onDelete={deletePlant}/>}
             {openAdd && <AddPlant onAdd={addPlant} hideAdd={handleOpenAdd}/>}
+
         </>
     );
 }
--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from "react";
-import {API} from '../home/home';
+//import {API} from '../home/home';
 import {Link} from "react-router-dom";
 import {AddDiary} from "./addDiary";
 import {EditPlant} from "./editProfile";
+import firebase from "firebase";
+import {db} from "../firebase";
 
 
 export const Profile = ({match}) => {
@@ -15,6 +17,48 @@ export const Profile = ({match}) => {
     }, [])
 
     const getPlant = () => {
+        db.collection('plants')
+            .doc(`${match.params.plantId}`)
+            .get()
+            .then(plt => {
+                console.log('plt from firebase::', plt.data());
+                setPlant(plt.data());
+            })
+            .catch(error => console.error('Err', error))
+        console.log('plant id do firebase', plant.id);
+        console.log('plant po add diary::', plant);
+    }
+
+    const addDiary = (newDiary) => {
+        db.collection('plants')
+            .doc(`${match.params.plantId}`)
+            .update({diary: firebase.firestore.FieldValue.arrayUnion(newDiary)})
+            .then(() => {
+                setPlant({
+                    ...plant,
+                    diary: [...plant.diary, newDiary]
+                })
+            })
+            .catch(error => console.error('Err', error))
+    }
+
+    const updatePlant = (plantData) => {
+        db.collection('plants')
+            .doc(`${match.params.plantId}`)
+            .update({
+                name: plantData.name,
+                species: plantData.species,
+                date: plantData.date,
+                care: plantData.care
+            })
+            .then(() => {
+                setPlant(plantData)
+            })
+            .catch(error => console.error('Err', error))
+    }
+
+
+    /*const getPlant = () => {
         fetch(`${API}/plants/${match.params.plantId}`, {
             method: 'GET'
         })
@@ -24,11 +68,11 @@ export const Profile = ({match}) => {
                 setPlant(plt);
             })
             .catch(err => console.log('Err', err));
-    }
+    }*/
 
     console.log('profile/plant::', plant);
 
-    const addDiary = (newDiary) => {
+    /*const addDiary = (newDiary) => {
         fetch(`${API}/plants/${plant.id}`, {
             method: 'PATCH',
             headers: {
@@ -41,22 +85,22 @@ export const Profile = ({match}) => {
                 setPlant(diary);
             })
         console.log('plant po add diary::', plant);
-    }
+    }*/
 
-    const updatePlant = (plantData) => {
-        fetch(`${API}/plants/${plant.id}`, {
-            method: 'PATCH',
-            headers: {
-                "Content-TYpe": "application/json"
-            },
-            body: JSON.stringify(plantData)
-        })
-            .then(resp => resp.json())
-            .then(updatedPlant => {
-                console.log('updatedPlant', updatedPlant);
-                setPlant(updatedPlant);
-            })
-    }
+    /* const updatePlant = (plantData) => {
+         fetch(`${API}/plants/${plant.id}`, {
+             method: 'PATCH',
+             headers: {
+                 "Content-TYpe": "application/json"
+             },
+             body: JSON.stringify(plantData)
+         })
+             .then(resp => resp.json())
+             .then(updatedPlant => {
+                 console.log('updatedPlant', updatedPlant);
+                 setPlant(updatedPlant);
+             })
+     }*/
 
 
     const showAdd = todo => {
@@ -75,7 +119,7 @@ export const Profile = ({match}) => {
 
             <button className='profile__edit__btn' onClick={() => showEdit(true)}>Edytuj profil</button>
 
-            {openEdit && <EditPlant plant={plant} onUpdatePlant={updatePlant} hideAdd={showEdit} />}
+            {openEdit && <EditPlant plant={plant} onUpdatePlant={updatePlant} hideAdd={showEdit}/>}
             {openAdd && <AddDiary onAddDiary={addDiary} hideAdd={showAdd} plant={plant}/>}
 
             <div className='profile__diary'>
