11a437e31f282ae0586084cac448251467cacd41
AdLap <a.lapacz@global4net.com>
2023-04-03 20:40:55 +0200

    GROW-1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import './App.scss';
-import {Home} from "./home/home";
-import {Profile} from "./profile/profile"
-import {Gallery} from "./gallery/gallery";
-import {
-    HashRouter,
-    Route,
-    Link,
-    Switch,
-} from 'react-router-dom';
+import './App.scss'
+import { Home } from './home/home'
+import { Profile } from './profile/profile'
+import { Gallery } from './gallery/gallery'
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
 
 function NotFound() {
-    return (
-        <>
-            <h1>Strona nie istnieje</h1>
-            <Link to='/'>Home</Link>
-        </>
-    );
+	return (
+		<>
+			<h1>Strona nie istnieje</h1>
+			<Link to='/'>Home</Link>
+		</>
+	)
 }
 
 function App() {
-    return <HashRouter>
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/profile/:plantId' component={Profile}/>
-            <Route path='/gallery' component={Gallery} />
-            <Route component={NotFound}/>
-        </Switch>
-    </HashRouter>
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/profile/:plantId' element={<Profile />} />
+				<Route path='/gallery' element={<Gallery />} />
+				<Route element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
