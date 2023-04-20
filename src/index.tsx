70da56ad994560d7249f9399fd2738bd154e8cb6
AdLap <a.lapacz@global4net.com>
2023-04-20 15:52:32 +0200

    GROW-17

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,10 @@ import './index.scss'
 import App from './App'
 
 const container = document.getElementById('root')
-const root = createRoot(container)
+const root = createRoot(container!)
 
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>
 )
